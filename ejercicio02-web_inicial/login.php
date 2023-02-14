98630c68a9284995d14d90dc5c10678cfdb8e473
dgallee <100680484+dgallee@users.noreply.github.com>
2023-02-14 16:43:03 +0100

    Add files via upload

---
+++ ejercicio02-web_inicial/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title> 
+    <style type="text/css">
+    h1 { text-align: center; }
+    td { padding: 0.2em 2em ; }
+    </style>
+</head>
+<body>
+    <h1>Registro/Login.</h1>
+    <form action="comprobar.php" method="post"/>
+        <p>Nombre:    <input type="text" name="nombre"/></p>
+        <p>Contraseña: <input type="password" name="contras"/><br/>
+    </form>
+</body>
+</html>
+
