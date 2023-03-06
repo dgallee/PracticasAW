5eb5441762ed4021b6d8f58e415295990c3e412a
dgallee <100680484+dgallee@users.noreply.github.com>
2023-03-06 11:44:51 +0100

    Update index.php

--- ejercicio03/index.php
+++ ejercicio03/index.php
@@ -1,1 +1,30 @@
-
+<?php
+//Inicio del procesamiento
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Portada</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+</head>
+<body>
+<div id="contenedor">
+<?php
+require('cabecera.php');
+require('sidebarIzq.php');
+?>
+	<main>
+		<article>
+			<h1>Página principal</h1>
+			<p> Aquí está el contenido público, visible para todos los usuarios. </p>
+		</article>
+	</main>
+<?php
+require('sidebarDer.php');
+require('pie.php');
+?>
+</div>
+</body>
+</html>
